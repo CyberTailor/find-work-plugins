8569dee49bf9b9f68c295402ba168263e551b8fa
Anna “CyberTailor” <cyber@sysrq.in>
2024-12-02 20:29:13 +0500

    find-work-example: add for boilerplate

---
+++ find-work-example/find_work/plugins/example/constants.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: WTFPL
+# SPDX-FileCopyrightText: 2024 Anna <cyber@sysrq.in>
+# No warranty
+
+"""
+All important constants in one place.
+"""
---
+++ find-work-example/find_work/plugins/example/types.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: WTFPL
+# SPDX-FileCopyrightText: 2024 Anna <cyber@sysrq.in>
+# No warranty
+
+"""
+Internal type definitions.
+"""
